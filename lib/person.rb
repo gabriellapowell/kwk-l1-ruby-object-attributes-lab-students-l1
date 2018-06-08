aa79bc3f203fc7908a1aa51d0000d5342e00db13
gabriellapowell <soccerplayer60@icloud.com>
2018-06-08 19:59:48 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -15,11 +15,11 @@ class Person
     puts job
   end
   
-  def name = (new_name)
+  def name= (new_name)
     @name = new_name
   end
   
-  def job = (new_job)
+  def job= (new_job)
     @job = new_job
   end
 end
