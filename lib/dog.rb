a03d760a1cbdf3da5a05f1544ea833e1823a3aa9
gabriellapowell <soccerplayer60@icloud.com>
2018-06-08 19:58:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
 # dog.rb
+class Dog 
+  def initialize(name, breed)
+    @name
+    @breed
+  end
+  
+  def name
+    @name = name
+    puts name}
+  end
+  
+  def breed 
+    @breed = breed
+    puts breed
+  end
+  
+  def name = (new_name)
+    @name = new_name
+  end
+  
+  def breed = (new_breed)
+    @breed = new_breed
+  end
+end
+
+fido = Dog.new 
+fido.name = "Fido"
+
+
+puts fido.name
+
+# lassie = Dog.new 
+# lassie.name = "Lassie"
+
+# puts lassie.name 
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,27 @@
 # person.rb
+class Person 
+  def initialize(name, job)
+    @name
+    @job
+  end
+  
+  def name
+    @name = name
+    puts name
+  end
+  
+  def job
+    @job = job
+    puts job
+  end
+  
+  def name = (new_name)
+    @name = new_name
+  end
+  
+  def job = (new_job)
+    @job = new_job
+  end
+end
+
+Person.new = ("Beyonce", "singer")
